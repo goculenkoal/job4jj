f222fefa40f11ddb081c84bbecf94643591bbf5b
alex <goculenko.al@gmail.com>
2021-05-20 21:41:59 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Tracker {
     private final Item[] items = new Item[100];
@@ -20,13 +21,14 @@ public class Tracker {
 
     /**
      * Метод получения всех заявок.
+     *
      * @return возврат массива result без null элементов.
      */
     public Item[] findAll() {
         Item[] result = new Item[size]; // создание массива result размером size
         int count = 0; //счетчик нового массива
         for (int index = 0; index < size; index++) { //луп для прохода по массиву
-             Item item = items[index]; //запись i - го объекта  из массива items;
+            Item item = items[index]; //запись i - го объекта  из массива items;
             if (item != null) {
                 result[count] = item;
                 count++;
@@ -37,6 +39,7 @@ public class Tracker {
 
     /**
      * Метод получения списка заявок, у которых совпадает имя.
+     *
      * @param name - имя заявки
      * @return - возврат массива result без null элементов.
      */
@@ -55,18 +58,35 @@ public class Tracker {
 
     /**
      * Метод поиска по ID
+     *
      * @param id - поиск по данному id
      * @return result - возвращаем найденную заявку, если заявки не существует - возвращаем - null;
      */
     public Item findById(int id) {
-        Item rsl = null;
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+       int index = indexOf(id);
+       if (id != -1) {
+           item.setId(id);
+           items[index] = item;
+           return true;
+       }
+       return false;
     }
 }
