288503bea2e0b1d415bddc9b3f74619c12112528
alex <goculenko.al@gmail.com>
2021-09-20 20:19:16 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -149,5 +149,27 @@ public class StartUITest {
                         + "0. Exit" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() throws IOException {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"100", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAct(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu: " + ln
+                                + "0. Exit" + ln
+                                + "Wrong input, you can select: 0 .. 0" + ln
+                                + "Menu: " + ln
+                                + "0. Exit" + ln
+                )
+        );
+    }
 }
 
