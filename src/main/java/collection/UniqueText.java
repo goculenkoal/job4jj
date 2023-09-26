d8104a02f14a27dda66a90493554c66391553c10
alex <goculenko.al@gmail.com>
2023-09-26 19:29:40 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String textCh: origin) {
+            check.add(textCh);
+        }
+        for (String chText:text) {
+            if(!(check.contains(chText))) {
+                rsl = false;
+                break;
+            }
+        }
+        /* for-each origin -> new HashSet. */
+        /* for-each text -> hashSet.contains */
+        return rsl;
+    }
+}
